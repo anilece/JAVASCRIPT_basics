633ba5806275adfbf88e86c234e5809cf2a77353
Anil_Kumar_R <47322405+anilece@users.noreply.github.com>
2020-07-21 11:21:17 +0530

    Update Functional_filter.js

--- Functional_filter.js
+++ Functional_filter.js
@@ -18,3 +18,18 @@ Array.prototype.myFilter = function(callback){
 var new_s = s.myFilter(function(item){
   return item % 2 === 1;
 });
+==================================================================================
+  const squareList = (arr) => {
+  // Only change code below this line
+  var res=[]
+  let filtr = arr.filter((i)=>{
+    if (i>0 && Math.round(i)===i){
+      res.push(i*i);
+    }
+  })
+  return res;
+  // Only change code above this line
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
